088da91676a5bfe94c9ffa28305d051aea3f1c3f
Baijayanti Priyadarshini <baijayantidash99@gmail.com>
2023-06-23 15:44:35 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Session5_BoilerPlate.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Session5_BoilerPlate.Controllers
+{
+    [Route("api/Auth")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private Users AuthenticateUser(Users user)
+        {
+            Users _user = null;
+            if(user.UserName == "admin" && user.Password == "Abc@123")
+            {
+                _user = new Users { UserName = "Baijayanti" }; 
+                
+            }
+            return _user;
+        }
+
+        private string GenerateToken(Users user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"],null,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Users users)
+        {
+            IActionResult response = Unauthorized();
+            var user_ = AuthenticateUser(users);
+
+            if(user_  != null)
+            {
+                var token = GenerateToken(user_);
+                response = Ok(new {token = token});
+            }
+            return response;
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace Session5_BoilerPlate.Models
+{
+    public class Users
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
